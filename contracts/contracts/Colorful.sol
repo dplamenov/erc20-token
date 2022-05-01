27b01359a133b01102b7410655e541d71d522d82
dplamenov <dimitar.plamenov@gmail.com>
2022-05-01 19:05:41 +0300

    Update Colorful.sol

--- contracts/contracts/Colorful.sol
+++ contracts/contracts/Colorful.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Colorful is ERC20 {
     constructor() ERC20("Colorful", "COL") {
-        _mint(msg.sender, 100000 * 10 * 18);
+        _mint(msg.sender, 1000 * 10**18);
     }
 }

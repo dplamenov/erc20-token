492e37bfb4535576b57ba413dd3d68454c8f76c7
dplamenov <dimitar.plamenov@gmail.com>
2022-05-01 12:29:07 +0300

    connect with contract

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
+import { ethers } from 'ethers';
+import ERC20 from './abi/ERC20.json';
 
 function App() {
+  const [currentAccount, setCurrentAccount] = useState();
+
+  useEffect(() => {
+    window.ethereum.request({ method: 'eth_requestAccounts' })
+      .then(result => {
+        setCurrentAccount(result[0]);
+    }, [])
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const address = e.target.address.value;
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const erc20 = new ethers.Contract(address, ERC20, provider);
+
+    const name = await erc20.name();
+    console.log(name);
+  }; 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Read data from ERC20 contract
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <input placeholder="contract address" name="address" id="address"/>
+        <button>Read</button>
+      </form>
+      <p>
+        Name: <span>name</span>
+      </p>
+      <p>
+        Symbol: <span>symbol</span>
+      </p>
+      <p>
+        Total supply: <span>totalSupply</span>
+      </p>
     </div>
   );
 }
